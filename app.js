dbff463ea46573198ce069baf0cb76a3afc26a68
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-11-15 10:16:27 +0100

    Update for quest 4!

--- app.js
+++ app.js
@@ -16,8 +16,23 @@ app.get("/", welcome);
 
 const userHandlers = require("./userHandlers");
 
-app.get("/api/users", userHandlers.postUser);
+app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
+app.post("/api/users/:id", userHandlers.getUserById);
+app.put("/api/users/:id", userHandlers.updateUser);
+
+//movies
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/movies/:id", movieHandlers.getMovieById);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -26,20 +41,3 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
-
-//movies
-
-// const movieHandlers = require("./movieHandlers");
-
-// app.get("/api/movies", movieHandlers.postMovie);
-// app.get("/api/movies/:id", movieHandlers.getMovieById);
-
-// app.listen(port, (err) => {
-//   if (err) {
-//     console.error("Something bad happened");
-//   } else {
-//     console.log(`Server is listening on ${port}`);
-//   }
-// });
-
-
